25ca7ff55869e7d124d6060b99a07557d505726b
Gabriel2898 <gabriel2898cc@gmail.com>
2021-04-28 11:37:41 -0500

    se agregaron los archivos

---
+++ CAPA_PRESENTACION/index.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LOGIN.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="CAPA_PRESENTACION.WebForm1" %>
---
+++ CAPA_PRESENTACION/index.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CAPA_PRESENTACION
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["sesionNombre"].ToString() != "gabriel" && Session["sesionApellido"].ToString() != "cadena") 
+                {
+
+
+                    Response.Redirect("administrador.aspx");
+
+                } else{
+                    txtnombre.Text = Session["sesionNombre"].ToString;
+                    txtcontraseña.Text = Session["sesionApellido"].ToString;
+
+                }                catch (Exception)
+
+            {
+                Response.Redirect("administrador.aspx");
+            }
+        }
+
+
+    }
+}
